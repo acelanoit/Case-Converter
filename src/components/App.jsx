55961312662c2b8457c3256acd1e0f76777cfb13
Antonio Celano <snake8701@hotmail.com>
2022-10-24 14:13:09 +0200

    Add footer (copyright notice)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TextArea from "./TextArea";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div>
             <Header />
             <TextArea />
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+
+    return <p class="mt-5 mb-3 text-muted">Copyright © 2022 Antonio Celano</p>
+
+}
+
+export default Footer;
